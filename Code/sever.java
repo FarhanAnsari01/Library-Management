bcd90e8dbd7bdd271f008d09b3ef1d4b30bd24f8
FarhanAnsari01 <iimtworking@gmail.com>
2024-09-27 12:48:35 +0530

    All most comp

---
+++ Code/sever.java
@@ -0,0 +1,27 @@
+///*
+// * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+// * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+// */
+//package library;
+//import java.sql.*;
+///**
+// *
+// * @author Farhan
+// */
+//public class sever
+//{
+//   public static Connection getCon()
+//   {
+//        try
+//        {
+//            Class.forName("com.mysql.cj.jdbc.Driver");
+//            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/libproject","root","fhr9h3zkx6");
+//            return con;
+//        }
+//        catch(Exception e)
+//        {
+//            System.out.println("Connection error");
+//            return null;
+//        }
+//   }
+//}
